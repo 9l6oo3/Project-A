9bfd3148db5622ac802d3725e6fa111592d4b722
sultanabalfoura <sultanabalfoura@gmail.com>
2023-12-01 20:03:28 +0400

    added

---
+++ Day2/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Day2
+{
+    class Progam
+    {
+        static void Main()
+        {
+
+            //Identify and Initializing variables
+            //<datatype> <variablename> = <value> ;
+
+            string FullName = "Sultana Balfoura" , job = "Full stack developer trainee";
+            //string job = "Full stack developer trainee ";
+            
+            int age = 25;
+
+
+            //print massage with space and calling variables 
+            Console.WriteLine(" FullName :" + FullName + "\n job :" + job + "\n age : " + age);
+
+
+            Console.WriteLine("hello there \n" +
+                "\t I am in patch 3");
+
+
+
+            //one line comment
+            /* 
+             
+             multi line comments
+             
+             */
+
+
+            //identify double, float, and decimal
+            double payment = 3.45;
+            float money = 44.5f;
+            decimal salary = 3.4m;
+
+
+            //checked & unchecked Bolck
+
+            // checked block
+
+            /*checked
+            {
+                long x = 1234587653;
+                int update = int(x);
+
+            }*/
+
+            int intMax = int.MaxValue;
+            Console.WriteLine("Max integer : " + (intMax + 2));
+
+            checked
+            {
+                intMax = int.MaxValue;
+                Console.WriteLine("Inside checked Block : " + (intMax + 1));
+            }
+
+            unchecked
+            {
+                intMax = int.MaxValue;
+                Console.WriteLine("Inside unchecked Block: " + intMax + 2);
+            }
+
+
+
+
+
+            //identify object and print referece in heap
+            Object obj1 = new Object();
+            Object obj2 = new Object();
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+
+            obj1 = obj2;
+
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+
+
+
+        }
+
+    }
+}
---
+++ Day6 solution/Day6/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Day6;
+class Program
+{
+    static void PrintLine(int length , string pattren , bool flag)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            if (flag == true)
+                Console.WriteLine(pattren);
+            else
+                Console.Write(pattren);
+        }
+    }
+
+    static void swap(int x, int y)
+    {
+        int temp = x;
+        x = y;
+        y = temp;
+        Console.WriteLine($"sawpping  with value complete {x} , {y} ");
+    }
+
+    static void swapRef(ref int x, ref int y)
+    {
+        int temp = x;
+        x = y;
+        y = temp;
+        Console.WriteLine($"sawpping  with reference complete {x} , {y} ");
+
+    }
+
+
+    static void Main(string[] args)
+    {
+        /*
+        Console.WriteLine("Enter number of pattren :");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("enter pattren you want : ");
+        string pattren = Console.ReadLine();
+
+        Console.WriteLine("print in new line (false/ true)? ");
+        bool flagNew = Convert.ToBoolean(Console.ReadLine());
+
+        PrintLine(n, pattren, flagNew);
+        */
+
+        int a = 6;
+        int b = 4;
+
+        swap(a, b);
+        Console.WriteLine($"in main method the value a {a}, the value of b {b}");
+        Console.WriteLine("----------------------------------");
+        swapRef(ref a, ref b);
+        Console.WriteLine($"in main method the value a {a}, the value of b {b}");
+
+    }
+
+}
+

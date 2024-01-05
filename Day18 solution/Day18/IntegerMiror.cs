6cc450de92f7c9ec3514b2384055d5f552faf69c
sultanabalfoura <sultanabalfoura@gmail.com>
2024-01-05 11:24:01 +0400

    new

---
+++ Day18 solution/Day18/IntegerMiror.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Day18
+{
+	public static class IntegerMiror
+	{
+		public static int Miror(this int number)
+		{
+			int result = 0, rem;
+			while(number != 0)
+			{
+				rem = number % 10;
+				result = result * 10 + rem;
+				number = number / 10;
+			}
+			return result;
+		}
+	}
+}
+
---
+++ Day18 solution/Day18/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Day18;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //extension method 
+        //int x = 12345;
+        //Console.WriteLine(x.Miror());
+
+
+        List<int> lst = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        var result = lst.Where( x => x % 2 != 0);
+        //List<int> result = lst.Where(x => x % 2 != 0).ToList();
+        foreach (var x in result)
+        {
+            Console.WriteLine(x);
+        }
+        //add new elements in list
+
+        Console.WriteLine("------------------------------------------------------------");
+        // it's Enumarable type
+        // to stop Enumarable => add .toList() in where condition
+        //var result = lst.Where( x => x % 2 != 0).ToList(); => now will stop connection 
+        lst.AddRange(new int[] { 10, 11, 12, 13, 14 });
+        lst.Remove(2);
+        foreach (var item in lst)
+        {
+            Console.WriteLine($"{item}");
+        }
+
+    }
+}
+
